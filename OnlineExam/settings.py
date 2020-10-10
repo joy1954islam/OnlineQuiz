f2368e1d15abf08c1df5792463837340671772c8
joyislam1954 <joyislam1954@gmail.com>
2020-10-10 12:45:46 +0600

    online exam

--- OnlineExam/settings.py
+++ OnlineExam/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
-
+    'django.contrib.humanize',
     'crispy_forms',
     'accounts',
     'SuperAdmin',
